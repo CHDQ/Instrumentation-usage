45e05e46ef4227f808079a903664f9280d83e02c
qiong.duan <qiong.duan@langtaojin.com>
2019-03-18 19:06:41 +0800

    new code

--- agent/src/main/java/org/dq/transfor/PreMain.java
+++ agent/src/main/java/org/dq/transfor/PreMain.java
@@ -4,6 +4,6 @@ import java.lang.instrument.Instrumentation;
 
 public class PreMain {
     public static void premain(String agentArgs, Instrumentation _inst) {
-        _inst.addTransformer(new MyTransfor());
+        _inst.addTransformer(new MyTransfor(), true);//需要第二个参数
     }
 }
