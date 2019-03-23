98ee2220f3dc3b790848bae288db9b07ba907d97
qiong.duan <super.duanqiong@gmail.com>
2019-03-23 19:43:56 +0800

    fix

--- simple-application/src/main/java/org/dq/App.java
+++ simple-application/src/main/java/org/dq/App.java
@@ -4,8 +4,6 @@ package org.dq;
  * Hello world!
  */
 public class App {
-    private static final String LOCK = "lock";
-
     public static void main(String[] args) {
         while (true) {
             new Test().myTest();
